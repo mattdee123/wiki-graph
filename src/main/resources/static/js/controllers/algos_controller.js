3bbe178418323c1d024fb31e6266d0f21455733c
Aakash Patel <aakpat6@gmail.com>
2013-11-18 12:07:47 -0500

    Add loading icon for shortest path.

--- src/main/resources/static/js/controllers/algos_controller.js
+++ src/main/resources/static/js/controllers/algos_controller.js
@@ -1,13 +1,17 @@
 WG.controller('AlgosController', function($scope, Fetch) {
   $scope.form = {};
+  $scope.shortestPathLoading = false;
 
   $scope.shortestPath = function() {
+    $scope.shortestPathLoading = true;
     Fetch.getShortestPath($scope.shortestPath.start, $scope.shortestPath.end,
       function(result) {
+        $scope.shortestPathLoading = false;
         $scope.shortestPathResult = result;
       },
       function(error) {
-        console.log(error);
+        $scope.shortestPathLoading = false;
+        $scope.shortestPathResult = [];
       }
     );
   };
