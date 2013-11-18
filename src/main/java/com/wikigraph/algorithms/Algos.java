23c6151b18f6c14c3ca0ffd90188a2a4c9d5dae2
Aakash Patel <aakpat6@gmail.com>
2013-11-18 12:02:59 -0500

    Fix imports.

--- src/main/java/com/wikigraph/algorithms/Algos.java
+++ src/main/java/com/wikigraph/algorithms/Algos.java
@@ -1,11 +1,18 @@
 package com.wikigraph.algorithms;
 
-
 import com.google.common.base.Stopwatch;
 import com.google.common.collect.Lists;
 import com.wikigraph.graph.Article;
 
-import java.util.*;
+import java.util.ArrayDeque;
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
+import java.util.Queue;
+import java.util.Set;
 
 import static java.util.concurrent.TimeUnit.MILLISECONDS;
 
